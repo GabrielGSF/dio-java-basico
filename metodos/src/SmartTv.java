36a5ee95cc0ca9d9f31b98f1111c70291200501b
GabrielGSF <gabrielsantos.gsf@gmail.com>
2023-08-21 22:04:51 -0300

    refatorar output

--- metodos/src/SmartTv.java
+++ metodos/src/SmartTv.java
@@ -6,31 +6,38 @@ public class SmartTv {
     public void mudarEstatus(String comando) {
         if (comando == "on") {
             estatus = true;
+            System.out.println("Estatus Mudou -> Estatus Atual: " + estatus);
         }
         if (comando == "off") {
             estatus = false;
+            System.out.println("Estatus Mudou -> Estatus Atual: " + estatus);
         }
     }
 
     public void trocarCanal(int canalNovo) {
         canal = canalNovo;
+        System.out.println("Novo Canal -> Canal Atual:" + canal);
     }
 
     public void mudarCanal(String comando) {
         if (comando == ">") {
             canal++;
+            System.out.println("Canal Aumentou -> Canal Atual: " + canal);
         }
         if (comando == "<") {
             canal--;
+            System.out.println("Canal Abaixou -> Canal Atual: " + canal);
         }
     }
 
     public void mudarVolume(String comando) {
         if (comando == "+") {
             volume++;
+            System.out.println("Volume Aumentou -> Volume Atual: " + volume);
         }
         if (comando == "-") {
             volume--;
+            System.out.println("Volume Abaixou -> Volume Atual: " + volume);
         }
     }
 }
--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -6,38 +6,23 @@ public class Usuario {
 
         System.out.println("Estatus Atual:" + smartTv.estatus);
         System.out.println("Canal Atual:" + smartTv.canal);
-        System.out.println("Volume Atual ? :" + smartTv.volume);
+        System.out.println("Volume Atual:" + smartTv.volume);
 
         System.out.println("-------------------------");
     
         smartTv.mudarEstatus("on");
-        System.out.println("Estatus Mudou -> Estatus Atual: " + smartTv.estatus);
-
         smartTv.mudarEstatus("off");
-        System.out.println("Estatus Mudou -> Estatus Atual: " + smartTv.estatus);
-
         smartTv.mudarEstatus("on");
-        System.out.println("Estatus Mudou -> Estatus Atual: " + smartTv.estatus);
 
         smartTv.trocarCanal(40);
-        System.out.println("Novo Canal -> Canal Atual:" + smartTv.canal);
-
+        
         smartTv.mudarCanal("<");
-        System.out.println("Canal Abaixou -> Canal Atual: " + smartTv.canal);
-
         smartTv.mudarCanal(">");
-        System.out.println("Canal Aumentou -> Canal Atual: " + smartTv.canal);
-
         smartTv.mudarCanal(">");
-        System.out.println("Canal Aumentou -> Canal Atual: " + smartTv.canal);
 
         smartTv.mudarVolume("-");
-        System.out.println("Volume Abaixou -> Canal Atual: " + smartTv.volume);
-
         smartTv.mudarVolume("+");
-        System.out.println("Volume Aumentou -> Canal Atual: " + smartTv.volume);
-
         smartTv.mudarVolume("+");
-        System.out.println("Volume Aumentou -> Canal Atual: " + smartTv.volume);
+   
     }
 }
