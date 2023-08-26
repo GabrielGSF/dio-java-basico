7fc57e6eac70b23228bdc1ba44b07791ad66716c
GabrielGSF <gabrielsantos.gsf@gmail.com>
2023-08-25 23:39:39 -0300

    classes exemplos

---
+++ poo/classes/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+    
+    void eating(Lunch lunch) {
+
+    }
+
+    void drinking(Juice juice) {
+        
+    }
+
+    void running() {
+
+    }
+}
+
